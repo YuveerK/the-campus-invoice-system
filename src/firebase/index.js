5cd5f601bfa772bcc44a37bc0ead1dc20e7cbfb9
Yuveer Kallideen <yuveerkal01@gmail.com>
2023-04-19 03:03:42 +0200

    Added env variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,20 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-
+import dotenv from "dotenv";
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBFIeNXLtTsUmLCMw0FjIDFcekAZpufK8s",
-  authDomain: "the-campus-invoice-system.firebaseapp.com",
-  projectId: "the-campus-invoice-system",
-  storageBucket: "the-campus-invoice-system.appspot.com",
-  messagingSenderId: "169322290778",
-  appId: "1:169322290778:web:c0b923ce42a59c70724263",
-  measurementId: "G-V731E9NC7D",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
